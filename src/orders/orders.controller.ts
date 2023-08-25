7ec1f13df3413a30b99efbbb9e43681a9f543efc
todjwer13 <todjwr12@gmail.com>
2023-08-25 09:58:42 +0900

    fix: 외래키 설정 후 코드 수정

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -6,11 +6,11 @@ import {
   Patch,
   Post,
   Req,
-  Delete,
   UseGuards,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { AuthUser } from '../auth/auth.decorator';
 import { Request } from 'supertest';
 import { OrderCreateDto, OrderStatusDto } from './dtos';
 import { ResultableInterface } from 'src/common/interfaces';
@@ -42,39 +42,28 @@ export class OrdersController {
   getDetailOrder(@Param('order_id') order_id: number): Promise<any> {
     return this.ordersService.getDetailOrder(order_id);
   }
-  // //-- 주문 수정하기 --//
-  // @Patch('/:order_id')
-  // @UseGuards(AuthGuard)
-  // async updateOrder(
-  //   @Param('order_id') order_id: number,
-  //   @Body() data: OrderUpdateDto,
-  // ): Promise<ResultableInterface> {
-  //   return await this.ordersService.updateOrder(order_id, data);
-  // }
 
   // //-- 주문 취소하기 --//
   @Patch('/:order_id/cancel')
   @UseGuards(AuthGuard)
   async cancelOrder(
-    @Req() req: Request,
+    @AuthUser() authUser: RequestUserInterface,
     @Param('order_id') order_id: number,
   ): Promise<ResultableInterface> {
-    const authUser: RequestUserInterface = req['user'];
-    return await this.ordersService.cancelOrder(order_id, authUser);
+    return await this.ordersService.cancelOrder(authUser.user_id, order_id);
   }
   //-- 주문 부분 취소하기 --//
-  @Patch('/:order_id/:order_detail_id')
+  @Patch('/:order_id/:order_detail_id/selleccancel')
   @UseGuards(AuthGuard)
   async partialCancel(
-    @Req() req: Request,
+    @AuthUser() authUser: RequestUserInterface,
     @Param('order_id') order_id: number,
     @Param('order_detail_id') order_detail_id: number,
   ): Promise<ResultableInterface> {
-    const authUser: RequestUserInterface = req['user'];
     return await this.ordersService.partialCancel(
       order_id,
       order_detail_id,
-      authUser,
+      authUser.user_id,
     );
   }
 
@@ -89,4 +78,14 @@ export class OrdersController {
     const authUser: RequestUserInterface = req['user'];
     return await this.ordersService.sellerOrder(authUser, order_id, data);
   }
+
+  // //-- 주문 수정하기 --//
+  // @Patch('/:order_id')
+  // @UseGuards(AuthGuard)
+  // async updateOrder(
+  //   @Param('order_id') order_id: number,
+  //   @Body() data: OrderUpdateDto,
+  // ): Promise<ResultableInterface> {
+  //   return await this.ordersService.updateOrder(order_id, data);
+  // }
 }
