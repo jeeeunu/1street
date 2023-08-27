0a2da75923daf35e51581ded963139b8f0d98c0b
jeeeunu <dbwldms25@gmail.com>
2023-08-27 19:48:42 +0900

    fix: 상품설명 테이블 설정 변경

--- src/common/entities/products.entity.ts
+++ src/common/entities/products.entity.ts
@@ -25,7 +25,7 @@ export class ProductsEntity {
   public product_name: string;
 
   //-- 상품 설명 --//
-  @Column({ nullable: false })
+  @Column({ nullable: false, length: 4000 })
   @IsString()
   public product_desc: string;
 
