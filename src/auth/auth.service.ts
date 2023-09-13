a9ac68c2b78ce92326de7de0b540a3ce329f16e0
jeeeunu <dbwldms25@gmail.com>
2023-09-13 23:01:03 +0900

    fix: googleLogin auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -79,17 +79,28 @@ export class AuthService {
       newUser.profile_image = req.user.picture;
       newUser.provider = 'google';
       await this.usersRepository.save(newUser);
+
+      // JWT 토큰에 포함될 payload
+      const payload = {
+        user_id: newUser.id,
+        user_name: newUser.name,
+        email: newUser.email,
+      };
+
+      const accessToken = await this.jwtService.signAsync(payload);
+
+      return accessToken;
+    } else {
+      // JWT 토큰에 포함될 payload
+      const payload = {
+        user_id: user.id,
+        user_name: req.user.lastName,
+        email: req.user.email,
+      };
+
+      const accessToken = await this.jwtService.signAsync(payload);
+
+      return accessToken;
     }
-
-    // JWT 토큰에 포함될 payload
-    const payload = {
-      user_id: user.id,
-      user_name: req.user.lastName,
-      email: req.user.email,
-    };
-
-    const accessToken = await this.jwtService.signAsync(payload);
-
-    return accessToken;
   }
 }
